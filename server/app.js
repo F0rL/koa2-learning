54349b790a5f7d2ec386197d7b32ee9621c094d4
Forl <kumaloveslife@gmail.com>
2019-05-29 23:46:37 +0800

    解析JSON，提取中间件

--- server/app.js
+++ server/app.js
@@ -1,24 +1,9 @@
 const Koa = require('koa')
-const bodyParser = require('koa-bodyparser')
-const nunjucks = require('koa-nunjucks-2')
-const path = require('path')
-const staticFiles = require('koa-static')
-
-const app = new Koa()
+const middleware = require('./middleware')
 const router = require('./router')
 
-app.use(staticFiles(path.resolve(__dirname,'public')))
-
-app.use(nunjucks({
-  ext: 'html',
-  path: path.join(__dirname, 'views'),//指定视图目录
-  nunjucksConfig: {
-    trimBlocks: true //开启转义防XSS
-  }
-}))
-
-app.use(bodyParser())
-
+const app = new Koa()
+middleware(app)
 router(app)
 
 app.listen(3000, () => {
