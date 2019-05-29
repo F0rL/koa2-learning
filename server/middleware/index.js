73ab4708327e05cbba22819076307691f5443066
Forl <kumaloveslife@gmail.com>
2019-05-30 01:32:01 +0800

    处理错误

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -4,9 +4,15 @@ const bodyParser = require('koa-bodyparser')
 const nunjucks = require('koa-nunjucks-2')
 const staticFiles = require('koa-static')
 
+// 引入请求错误中间件
+const miHttpError = require('./mi-http-error')
+
 const miSend = require('./mi-send')
 const miLog = require('./mi-log')
 module.exports = (app) => {
+  app.use(miHttpError({
+    errorPageFolder: path.resolve(__dirname, '../errorPage')
+  }))
   app.use(miLog({
     env: app.env,  // koa 提供的环境变量
     projectName: 'koa2-tutorial',
@@ -25,4 +31,16 @@ module.exports = (app) => {
 
   app.use(bodyParser())
   app.use(miSend())
+
+  // 增加错误的监听处理
+  app.on("error", (err, ctx) => {
+    if (ctx && !ctx.headerSent && ctx.status < 500) {
+      ctx.status = 500
+    }
+    if (ctx && ctx.log && ctx.log.error) {
+      if (!ctx.state.logged) {
+        ctx.log.error(err.stack)
+      }
+    }
+  }) 
 }
