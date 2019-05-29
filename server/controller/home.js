6facf7ab2fecd28187c55d1585f5f0990cd03fd8
Forl <kumaloveslife@gmail.com>
2019-05-29 19:01:20 +0800

    格式化

--- server/controller/home.js
+++ server/controller/home.js
@@ -1,21 +1,20 @@
-const HomeService = require('../service/home')
+const HomeService = require("../service/home");
 
 module.exports = {
-  index: async(ctx, next) => {
-    ctx.response.body = `<h1>index page</h1>`
+  index: async (ctx, next) => {
+    ctx.response.body = `<h1>index page</h1>`;
   },
-  home: async(ctx, next) => {
-    console.log(ctx.request.query)
-    console.log(ctx.request.querystring)
-    ctx.response.body = '<h1>HOME page</h1>'
+  home: async (ctx, next) => {
+    console.log(ctx.request.query);
+    console.log(ctx.request.querystring);
+    ctx.response.body = "<h1>HOME page</h1>";
   },
-  homeParams: async(ctx, next) => {
-    console.log(ctx.params)
-    ctx.response.body = '<h1>HOME page /:id/:name</h1>'
+  homeParams: async (ctx, next) => {
+    console.log(ctx.params);
+    ctx.response.body = "<h1>HOME page /:id/:name</h1>";
   },
-  login: async(ctx, next) => {
-    ctx.response.body =
-      `
+  login: async (ctx, next) => {
+    ctx.response.body = `
       <form action="/user/register" method="post">
         <input name="name" type="text" placeholder="请输入用户名：kuma"/> 
         <br/>
@@ -23,14 +22,11 @@ module.exports = {
         <br/> 
         <button>GoGoGo</button>
       </form>
-    `
+    `;
   },
-  register: async(ctx, next) => {
-    let {
-      name,
-      password
-    } = ctx.request.body
-    let data = await HomeService.register(name, password)
-    ctx.response.body = data
+  register: async (ctx, next) => {
+    let { name, password } = ctx.request.body;
+    let data = await HomeService.register(name, password);
+    ctx.response.body = data;
   }
-}
+};
