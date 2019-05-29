fa243c91b5644523ab93716ad8921635f5d99862
Forl <kumaloveslife@gmail.com>
2019-05-29 15:36:51 +0800

    get post koa

--- query_router.js
+++ query_router.js
@@ -1,8 +1,10 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const bodyParser= require('koa-bodyparser')
 
 const app = new Koa()
 const router = new Router()
+app.use(bodyParser())
 
 router.get('/', async(ctx, next) => {
   ctx.response.body = `<h1>index page</h1>`
@@ -22,6 +24,29 @@ router.get('/user/:id/:name', async(ctx, next) => {
   ctx.response.body = `<h1>user page id:${params.id} name:${params.name}</h1>`
 })
 
+
+//返回表单路由
+router.get('/user', async(ctx, next)=>{
+  ctx.response.body = 
+  `
+    <form action="/user/register" method="post">
+      <input name="name" type="text" placeholder="请输入用户名：kuma"/> 
+      <br/>
+      <input name="password" type="text" placeholder="请输入密码：123456"/>
+      <br/> 
+      <button>GoGoGo</button>
+    </form>
+  `
+})
+//表单响应路由
+router.post('/user/register',async(ctx, next)=>{
+  let {name, password} = ctx.request.body
+  if( name === 'kuma' && password === '123456' ){
+    ctx.response.body = `Hello， ${name}！`
+  }else{
+    ctx.response.body = '账号信息错误'
+  }
+})
 
 
 app.use(router.routes())
