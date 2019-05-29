ea530b8882a9e57e1e01223ae900e29b2877dd85
Forl <kumaloveslife@gmail.com>
2019-05-29 19:27:48 +0800

    引入模板引擎

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const nunjucks = require('koa-nunjucks-2')
+const path = require('path')
+
 const app = new Koa()
 const router = require('./router')
+
+app.use(nunjucks({
+  ext: 'html',
+  path: path.join(__dirname, 'views'),//指定视图目录
+  nunjucksConfig: {
+    trimBlocks: true //开启转义防XSS
+  }
+}))
 
 app.use(bodyParser())
 
