9c71f44c167c7b71c07890e87824a37ae52ec2dd
Forl <kumaloveslife@gmail.com>
2019-05-29 14:29:35 +0800

    router.all

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const Router = require('koa-router')
 const app =  new Koa()
 const router =  new Router()
 
+// router.all('/*', async (ctx, next) => {
+//   ctx.set('Access-Control-Allow-Origin', '*')
+//   await next()
+// })
+
 router.get('/', async (ctx, next) => {
   ctx.response.body = '<h1>router page</h1>'
 })
@@ -10,6 +15,13 @@ router.get('/', async (ctx, next) => {
 router.get('/home', async (ctx, next) => {
   ctx.response.body = '<h1>router home page</h1>'
 })
+
+router.all('/*', async (ctx, next) => {
+  ctx.response.status = 404
+  ctx.response.body = '<h1>router 404</h1>'
+})
+
+
 // app.use(async (ctx, next) => {
 //   if(ctx.request.path === '/') {
 //     ctx.response.body = '<h1>index page</h1>'
